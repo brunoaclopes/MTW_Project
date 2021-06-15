60484a5c70cd3b1218b1e28239f1fd7690f70c59
LeandroFaria07 <43753346+LeandroFaria07@users.noreply.github.com>
2021-06-15 11:23:41 +0100

    Finish page Courses

--- WebUI/src/app/course/course-create/course-create.component.ts
+++ WebUI/src/app/course/course-create/course-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
+import {PeriodicElement} from "../course-list/course-list.component";
 
 @Component({
   selector: 'app-course-create',
@@ -8,15 +9,38 @@ import {HttpClient} from "@angular/common/http";
   styleUrls: ['./course-create.component.scss']
 })
 
-export class CourseCreateComponent {
+export class CourseCreateComponent implements OnInit{
 
-  constructor(private router: Router,
+  constructor(private route: ActivatedRoute,
+              private router: Router,
               private http: HttpClient) { }
 
   form = new Course('');
 
+  ngOnInit() {
+    let id = this.route.snapshot.paramMap.get('id');
+
+    if(id == null) return;
+
+    this.http.get<any[]>('http://localhost:8090/api/course/' + id)
+      .subscribe(data => {
+          console.log(data[0].Nome);
+          this.form.Nome = data[0].Nome;
+        },
+        error => {
+          console.log("error");
+        }
+      );
+  }
+
   onPost(){
-    this.http.post<Course>("http://localhost:8090/api/course", this.form).subscribe(value => {console.log(value)});
+    let id = this.route.snapshot.paramMap.get('id');
+
+    if(id == null)
+      this.http.post<Course>("http://localhost:8090/api/course", this.form).subscribe(value => {console.log(value)});
+    else
+      this.http.put<Course>("http://localhost:8090/api/course/" + id, this.form).subscribe(value => {console.log(value)});
+
 
     this.router.navigate(['/course']);
   }
